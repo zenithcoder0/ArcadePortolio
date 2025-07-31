394c7c100c2c0c260aa386135e9af083236f1dce
zenithcoder0 <zenithcoder0@gmail.com>
2025-07-31 13:14:28 -0500

    Main.tsx updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AppLayout } from "./layouts/AppLayout";
+import { MainMenu } from "./pages/MainMenu";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Placeholder pages:
+const Game = () => <div>Game Screen</div>;
+const About = () => <div>About Page</div>;
+const Resume = () => <div>Resume (Skill Tree?)</div>;
+const Credits = () => <div>Credits</div>;
+const NotFound = () => <div>404 - Page not found</div>;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AppLayout>
+        <Routes>
+          <Route path="/" element={<MainMenu />} />
+          <Route path="/game" element={<Game />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/credits" element={<Credits />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </AppLayout>
+    </BrowserRouter>
+  </React.StrictMode>
+);
